169a4f1e752d3f3f6626c416b350b8585c05b22c
Lucien Pereira <pereira.lucien@laposte.net>
2011-06-29 22:16:50 +0200

    SBT 0.1x migration.

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "4feun"
+
+version := "0.0.1"
+
+scalaVersion := "2.9.0-1"
+
+libraryDependencies ++= Seq("org.scalaz" % "scalaz-core_2.9.0-1" % "6.0.1")
---
+++ sbt.sh
@@ -0,0 +1,2 @@
+﻿!# /usr/bin/env sh
+java -Dfile.encoding=UTF8 -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256m -Xmx512M -jar `dirname $0`/sbt-launch.jar "$@"
